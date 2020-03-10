6820c8f082e4c6f1082e46230650becb4f190dd1
BearCub29 <poulichet.florian@gmail.com>
2020-03-10 15:21:37 +0100

    edit_profil + flash + front

--- e_cats/app/controllers/users_controller.rb
+++ e_cats/app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+  end
+  def update
+    @user = current_user
+      user_update = params.require(:user).permit(:first_name, :last_name, :description, :zip_code , :city)
+      if @user.update(user_update)
+        redirect_to edit_user_path
+        flash[:success] = "Les modifications sont bien prises en compte ;)"
+      else
+        render :edit
+      end
+  end
 end
