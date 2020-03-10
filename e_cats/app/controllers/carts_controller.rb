822c669407a0fd48b812ab5002666a4dd57c7f14
khatba <khatba.husain@gmail.com>
2020-03-10 17:31:21 +0100

    add cart ++

--- e_cats/app/controllers/carts_controller.rb
+++ e_cats/app/controllers/carts_controller.rb
@@ -1,11 +1,24 @@
 class CartsController < ApplicationController
+#  before_action :authenticate_user!
   def show
-    
+  #  @cart = Cart.where(user_id: current_user.id)
   end
-
 
   def index
-
-
   end
+  
+  def create
+    @cart = Cart.create(user_id: current_user.id)
+    JoinItemCart.create(cart_id: @cart.id, item_id: params[:item_id])
+  end
+
+
+  def new
+    puts "new" * 100
+  end
+
+
 end
+
+
+
--- e_cats/config/routes.rb
+++ e_cats/config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'users/index'
   devise_for :users
 
+  resources :carts 
+
+
 end
