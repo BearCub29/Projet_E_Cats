7a9c729e4a8f90fc5a1b26fd414ce1a8ab3cb68d
BearCub29 <poulichet.florian@gmail.com>
2020-03-11 11:05:34 +0100

    mailers

--- e_cats/app/models/user.rb
+++ e_cats/app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ApplicationRecord
-  after_create :welcome_send
-
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :items
-  
-  
-
+  after_create :welcome_send
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
-
-  
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
